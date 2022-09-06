1cd571666fa3f75e7a674a272b20b4cf675fc58a
Ibrahim Sule <ibraheemsulay@gmail.com>
2022-09-06 20:50:51 +0100

    built the comment section and made minor ui and fixes

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,8 +1,26 @@
 interface CommentProps {}
 import s from "../assets/scss/comment.module.scss";
+import avi from "../assets/images/Group 3.svg";
 
 const Comment: React.FC = () => {
-  return <div className={s.comment}>this is here</div>;
+  return (
+    <div className={s.comment}>
+      <p>Welcome to mars</p>
+
+      <div className={s.info}>
+        <span className={s.avi}>
+          <img src={avi} alt="user avi" />
+        </span>
+        <span className={s.name}>Arya Stark</span>
+        <span className={s.time}>1 day ago</span>
+      </div>
+      <div className={s.text}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam molestie
+        sem ut purus ornare, vel laoreet augue vestibulum. Ut cursus ex lacinia
+        dui blandit, at molestie elit luctus.
+      </div>
+    </div>
+  );
 };
 
 export default Comment;
--- src/components/User.tsx
+++ src/components/User.tsx
@@ -8,21 +8,26 @@ const User: React.FC = () => {
         <input type="checkbox" id="html" />
         <label htmlFor="html"></label>
       </div>
-      <div className={s.name}>
-        <span className={s.name_initial}>D</span>
-        <p>
-          Daniel James{" "}
-          {
-            <>
-              <span className={s.text}>has changed Status to </span>
-              <span className={s.status}>
-                <span className={s.status} style={{ background: "red" }}></span>
-                Completed
-              </span>
-              <span className={s.time}>1 day ago</span>
-            </>
-          }
-        </p>
+      <div className={s.activity}>
+        <div className={s.name}>
+          <span className={s.name_initial}>D</span>
+          <p>
+            Daniel James{" "}
+            {
+              <>
+                <span className={s.text}>has changed Status to </span>
+                <span className={s.status}>
+                  <span
+                    className={s.status}
+                    style={{ background: "red" }}
+                  ></span>
+                  Completed
+                </span>
+                <span className={s.time}>1 day ago</span>
+              </>
+            }
+          </p>
+        </div>
 
         <Comment />
       </div>
